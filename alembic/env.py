70b8737114ec793a938246a0907e836cd1506440
Angelito Chagas <chagasangelito@gmail.com>
2026-07-28 14:58:01 -0300

    chore(devtools): pin toolchain via mise and align ruff config with reference

--- alembic/env.py
+++ alembic/env.py
@@ -13,21 +13,7 @@ load_environment()
 
 config.set_main_option("sqlalchemy.url", get_database_url())
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-# if config.config_file_name is not None:
-#     fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Model.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
